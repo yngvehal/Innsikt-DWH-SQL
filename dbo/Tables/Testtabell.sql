f21ac9808fc65ce9c1287cddc0e667f9547e2e8f
yngvehal <yngve.haltbakk@rikstoto.no>
2021-10-26 13:16:24 +0200

    Add project files.

---
+++ dbo/Tables/Testtabell.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Testtabell] (
+    [Id]    INT            NULL,
+    [tekst] NVARCHAR (200) NULL
+);
+
---
+++ dbo/Tables/Testtabell_VS2019.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Testtabell_VS2019]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
+CREATE TABLE [dbo].[Testtabell] (
+    [Id]    INT            NULL,
+    [tekst] NVARCHAR (200) NULL
+);
+
